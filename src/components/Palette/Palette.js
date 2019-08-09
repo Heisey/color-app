78375fc488a3797b7e135e8d47dd7fe82095b2b0
justin heisler <admin@heiseymedia.com>
2019-08-09 01:23:45 -0600

    style Slider

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 // Components
 import ColorBox from '../ColorBox/ColorBox';
@@ -30,12 +30,15 @@ class Palette extends Component {
     ))
     return (
       <div className="Palette">
-        <Slider defaultValue={level}
-                min={100}
-                max={900}
-                step={100}
-                onAfterChange={this.changeLevel}
-        />
+        <div className='slider'>
+          <Slider defaultValue={level}
+                  min={100}
+                  max={900}
+                  step={100}
+                  onAfterChange={this.changeLevel}
+          />
+        </div>
+
         <div className="Palette-colors">
           {colorBoxes}
         </div>
