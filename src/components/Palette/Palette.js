ee60caa50c1e94b0bc0b4cf1b56cb67dade5a812
justin heisler <admin@heiseymedia.com>
2019-08-09 00:35:27 -0600

    colorHelper file created to generate colors using chroma.js

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './Palette.css';
+import { generatePalette } from '../../assets/colorHelpers';
+import seedColors from '../../assets/seedColors';
 
 // Components
 import ColorBox from '../ColorBox/ColorBox';
 
 class Palette extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]))
     const colorBoxes = this.props.colors.map(color => (
       <ColorBox background={color.color} name={color.name} />
     ))
