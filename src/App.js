c08a534b66e790bbf1b0d9d83b6fa3457f1fe40e
justin heisler <admin@heiseymedia.com>
2019-08-09 00:56:21 -0600

    added slider to UI to generate colors dynamicaly

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Palette from './components/Palette/Palette';
 
 // Assets
 import seedColors from './assets/seedColors';
+import { generatePalette } from './assets/colorHelpers';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
