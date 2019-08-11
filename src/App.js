fa1d19b5817a57e57f15ccad6b44b95970d7163a
justin heisler <admin@heiseymedia.com>
2019-08-10 22:35:12 -0600

    Router added, home and palette route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 
 // Components
 import Palette from './components/Palette/Palette';
@@ -9,12 +10,48 @@ import { generatePalette } from './assets/colorHelpers';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[1])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    const seedPalette = seedColors.find(palette => {
+      return palette.id === id
+    })
+    return seedPalette
+  }
+
+  consoleLog(param) {
+    console.log(param)
+  }
+
+  render() {
+      this.findPalette("flat-ui-colors-v1")
+    return (
+      <Switch>
+
+        <Route exact
+              path="/palette/:id"
+              render={routeProps => (
+                <Palette palette={generatePalette(
+                      this.findPalette(routeProps.match.params.id)
+                  )}
+                />
+              )}
+        />
+
+        <Route exact
+               to="/"
+               render={() => <h1>Palette List goes here</h1>}
+        />
+
+
+      </Switch>
+
+
+
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[1])} />
+      // </div>
+    )
+  }
 }
 
 export default App;
